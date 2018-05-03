9da38e12cbaa4f0313b06b196516aed85395a46c
Ricky <ricky@ohhdeer.com>
2018-05-03 11:47:54 +0100

    add install sh

---
+++ ghost-install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Testing full install of Ghost Blog for Ubuntu 16.04
+
+cd ~
+curl -sL https://deb.nodesource.com/setup_8.x -o nodesource_setup.sh
+bash nodesource_setup.sh
+apt-get install nodejs
+apt-get install build-essential
+apt-get install nginx
+apt-get install mysql-server
+mysql_secure_installation
+npm i -g ghost-cli
+mkdir -p /var/www/ghost
+chown $USER:$USER /var/www/ghost
+chmod 775 /var/www/ghost
+cd /var/www/ghost/
+ghost install
